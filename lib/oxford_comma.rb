6fe23b7a883758e1417cf74e000f9e36c14bdc37
active-object-6703 <nerajno@gmail.com>
2019-02-08 12:32:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,12 @@ def oxford_comma(array)
    array.join
  elsif array.length === 2
    array.join(" and ")
+ elsif array.length > 2
+   array[-1] = "and #{array[-1]}"
+   return array.join(", ")
+ end
+     
+   end
+   
  end
 end
